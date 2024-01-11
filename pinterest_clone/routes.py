59c03fc7f6b96d2c5aafd2ab7ca8209efafc58fc
Renan Teodoro <renant570@icloud.com>
2024-01-11 11:51:22 -0300

    feat: added functionality of login and logout with correct redirect

--- pinterest_clone/routes.py
+++ pinterest_clone/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, redirect
 from pinterest_clone import app, database, bcrypt
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from pinterest_clone.forms import LoginForm, RegistrationForm
 from pinterest_clone.models import User, Post
 
@@ -8,6 +8,11 @@ from pinterest_clone.models import User, Post
 @app.route("/", methods=["GET", "POST"])
 def home():
     loginForm = LoginForm()
+    if loginForm.validate_on_submit():
+        user = User.query.filter_by(email=loginForm.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, loginForm.password.data):
+            login_user(user)
+            return redirect(url_for("profile", user=user.username))
     return render_template("home.html", form=loginForm)
 
 
@@ -33,3 +38,10 @@ def create_account():
 @login_required
 def profile(user):
     return render_template("profile.html", user=user)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
