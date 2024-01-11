95ef8ee7b56b0d7a411d5f148290da5ed48661c9
Renan Teodoro <renant570@icloud.com>
2024-01-11 12:53:18 -0300

    feat: changed the route shown at browser and search done in database

--- pinterest_clone/routes.py
+++ pinterest_clone/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request
 from pinterest_clone import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
 from pinterest_clone.forms import LoginForm, RegistrationForm
@@ -12,7 +12,7 @@ def home():
         user = User.query.filter_by(email=loginForm.email.data).first()
         if user and bcrypt.check_password_hash(user.password, loginForm.password.data):
             login_user(user)
-            return redirect(url_for("profile", user=user.username))
+            return redirect(url_for("profile", username=user.username))
     return render_template("home.html", form=loginForm)
 
 
@@ -30,14 +30,21 @@ def create_account():
         database.session.commit()
         login_user(user, remember=True)
 
-        return redirect(url_for("profile", user=user.username))
+        return redirect(url_for("profile", username=user.username))
     return render_template("create-account.html", form=registrationForm)
 
 
-@app.route("/profile/<user>")
+@app.route("/profile/<username>")
 @login_required
-def profile(user):
-    return render_template("profile.html", user=user)
+def profile(username):
+    user_profile = User.query.filter_by(username=username).first()
+    userId = user_profile.id
+
+    if int(userId) == int(current_user.id):
+        return render_template("profile.html", user=current_user)
+
+    if int(userId) != int(current_user.id):
+        return render_template("profile.html", user=user_profile)
 
 
 @app.route("/logout")
